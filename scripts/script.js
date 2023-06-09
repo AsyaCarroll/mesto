1c5ffded134185a5ca35ec5cfed3c185ab166541
AsyaCarroll <carroll.anastasia@inbox.ru>
2023-06-09 22:49:00 +0400

    ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,20 @@
 const page = document.querySelector('.page');
 const editButton = page.querySelector('.profile__edit-button'); //кнопка изменения профиля
 const addButton = page.querySelector('.profile__add-button'); //кнопка добавления места
-let popUpProf = page.querySelector('.edit-prof'); //попап профиля
-let popUpPlace = page.querySelector('.add-place'); //попап добавления места
+const popUpProf = page.querySelector('.edit-prof'); //попап профиля
+const popUpPlace = page.querySelector('.add-place'); //попап добавления места
 //let popUpCloseButton = page.querySelectorAll('.popup__close'); кнопка закрытия попапа
 const formEdit = page.querySelector('.popup__edit-form'); //форма редактирования
 const formAdd = page.querySelector('.popup__add-form'); //форма добавления
 
-let nameInput = formEdit.querySelector('.popup__input_type_name'); //имя пользователя
-let jobInput = formEdit.querySelector('.popup__input_type_description'); //описание
-let profileName = page.querySelector('.profile__name');
-let profileDesc = page.querySelector('.profile__description');
-let placeName = page.querySelector('.popup__input_type_pic-name'); //инпут попапа - название места
-let placeLink = page.querySelector('.popup__input_type_link'); // инпут ссылка на картинку места
+const nameInput = formEdit.querySelector('.popup__input_type_name'); //имя пользователя
+const jobInput = formEdit.querySelector('.popup__input_type_description'); //описание
+const profileName = page.querySelector('.profile__name');
+const profileDesc = page.querySelector('.profile__description');
+const placeName = page.querySelector('.popup__input_type_pic-name'); //инпут попапа - название места
+const placeLink = page.querySelector('.popup__input_type_link'); // инпут ссылка на картинку места
 
 let allElements = page.querySelector('.elements');
-let elements = page.querySelectorAll('.element');
-let elName = page.querySelectorAll('.element__name');
-let elImg = page.querySelectorAll('.element__pic');
-
 const elementTemplate = page.querySelector('#element').content; //шаблон элемента
 
 const initialCards = [
@@ -48,40 +44,31 @@ const initialCards = [
     }
 ];
 
-const likeElements = Array.from(allElements.querySelectorAll('.element__like'));
-const deleteElements = Array.from(allElements.querySelectorAll('.element__trash'));
-
-let popUpIll = page.querySelector('.illustration');
-const popUpPic = Array.from(allElements.querySelectorAll('.element__pic'));
+function createCard(name, link) {
+    let elementAdded = elementTemplate.querySelector('.element').cloneNode(true);
+    elementAdded.querySelector('.element__pic').src = link;
+    elementAdded.querySelector('.element__pic').alt = name;
+    elementAdded.querySelector('.element__name').textContent = name;
+    return elementAdded;
+}
 
 function loadCards() {
-    elements.forEach((item, index) => {
-        elName[index].textContent = initialCards[index].name;
-        elImg[index].setAttribute('src', initialCards[index].link);
-        elImg[index].setAttribute('alt', initialCards[index].name);
+    initialCards.forEach((item) => {
+        allElements.prepend(createCard(item.name, item.link));
     })
 }
 
-function showPopUpProf() {
-    popUpProf.classList.add('popup_opened');
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileDesc.textContent;
+function showPopUp(element) {
+    console.log(element.target)
+    element.classList.add('popup_opened');
+    if (element == popUpProf) {
+        nameInput.value = profileName.textContent;
+        jobInput.value = profileDesc.textContent;
+    }
 }
 
-function showPopUpPlace() {
-    popUpPlace.classList.add('popup_opened');
-}
-
-function showPopUpCard(evt) {
-    popUpIll.classList.add('popup_opened');
-    page.querySelector('.popup__illustration-img').src = evt.target.src;
-    page.querySelector('.popup__illustration-desc').textContent = evt.target.parentElement.querySelector('.element__name').textContent;
-}
-
-function closePopUp() {
-    popUpProf.classList.remove('popup_opened');
-    popUpPlace.classList.remove('popup_opened');
-    popUpIll.classList.remove('popup_opened');
+function closePopUp(element) {
+    element.classList.remove('popup_opened');
 }
 
 function handleFormSubmit(evt) {
@@ -93,12 +80,23 @@ function handleFormSubmit(evt) {
 
 function handleAddFormSubmit(evt) {
     evt.preventDefault();
-    let elementAdded = elementTemplate.querySelector('.element').cloneNode(true);
-    elementAdded.querySelector('.element__pic').src = placeLink.value;
-    elementAdded.querySelector('.element__pic').alt = placeName.value;
-    elementAdded.querySelector('.element__name').textContent = placeName.value;
-    allElements.prepend(elementAdded);
-    closePopUp();
+    console.log(evt.target)
+    allElements.prepend(createCard(placeName.value, placeLink.value));
+    allElements = page.querySelector('.elements');
+    likeElements = Array.from(allElements.querySelectorAll('.element__like'));
+    likeElements[0].addEventListener('click', switchLike);
+    deleteElements = Array.from(allElements.querySelectorAll('.element__trash'));
+    deleteElements[0].addEventListener('click', deletePlace);
+    popUpPic = Array.from(allElements.querySelectorAll('.element__pic'));
+    popUpPic[0].addEventListener('click', function (evt) {
+        showPopUp(popUpIll);
+        page.querySelector('.popup__illustration-img').src = evt.target.src;
+        page.querySelector('.popup__illustration-desc').textContent = evt.target.parentElement.querySelector('.element__name').textContent;
+    });
+    console.log(popUpPic[0])
+    placeLink.value = '';
+    placeName.value = '';
+    closePopUp(evt.target.parentElement.parentElement);
 }
 
 function switchLike(evt) {
@@ -109,13 +107,25 @@ function deletePlace(evt) {
     evt.target.parentElement.parentElement.remove();
 }
 
+//не до конца поняла, как имеено следует навести порядок в коде. разложить по папкам функции, их вызовы и установку слушателей?
+
 loadCards();
-editButton.addEventListener('click', showPopUpProf);
-addButton.addEventListener('click', showPopUpPlace);
-popUpPic.forEach(item => item.addEventListener('click', showPopUpCard));
-popUpProf.querySelector('.popup__close').addEventListener('click', closePopUp);
-popUpPlace.querySelector('.popup__close').addEventListener('click', closePopUp);
-popUpIll.querySelector('.popup__close').addEventListener('click', closePopUp);
+
+let likeElements = Array.from(allElements.querySelectorAll('.element__like'));
+let deleteElements = Array.from(allElements.querySelectorAll('.element__trash'));
+let popUpPic = Array.from(allElements.querySelectorAll('.element__pic'));
+const popUpIll = page.querySelector('.illustration');
+
+editButton.addEventListener('click', function () { showPopUp(popUpProf) });
+addButton.addEventListener('click', function () { showPopUp(popUpPlace) });
+popUpPic.forEach(item => item.addEventListener('click', function (evt) {
+    showPopUp(popUpIll);
+    page.querySelector('.popup__illustration-img').src = evt.target.src;
+    page.querySelector('.popup__illustration-desc').textContent = evt.target.parentElement.querySelector('.element__name').textContent;
+}));
+popUpProf.querySelector('.popup__close').addEventListener('click', function () { closePopUp(popUpProf) });
+popUpPlace.querySelector('.popup__close').addEventListener('click', function () { closePopUp(popUpPlace) });
+popUpIll.querySelector('.popup__close').addEventListener('click', function () { closePopUp(popUpIll) });
 formEdit.addEventListener('submit', handleFormSubmit);
 formAdd.addEventListener('submit', handleAddFormSubmit);
 likeElements.forEach(item => item.addEventListener('click', switchLike));
